cdc786db4e60eba48fd77d5b25ff34f2cb57a770
rayk1m02 <hkapray@gmaill.com>
2025-02-01 18:13:21 -0600

    web vitals

--- client/src/reportWebVitals.js
+++ client/src/reportWebVitals.js
@@ -1,12 +1,6 @@
 const reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ 
-      getCLS,  // Cumulative Layout Shift - measures visual stability
-      getFID,  // First Input Delay - measures interactivity
-      getFCP,  // First Contentful Paint - measures loading performance
-      getLCP,  // Largest Contentful Paint - measures loading performance
-      getTTFB  // Time to First Byte - measures server response time
-    }) => {
+    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
