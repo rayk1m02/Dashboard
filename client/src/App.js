a596d2837c5ad9edc6d7609ed087a47a25c86ef6
rayk1m02 <hkapray@gmail.com>
2025-02-04 15:20:20 -0600

    adding new components to webpage

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,36 @@
 import './App.css';
-import StockDisplay from './components/StockDisplay';
+import StockChart from './components/StockChart';
+import StockList from './components/StockList';
+import SentimentAnalysis from './components/SentimentAnalysis';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Healthcare Stocks Dashboard</h1>
-        <StockDisplay />
+    <div className="min-h-screen bg-gray-100 p-4">
+      <header className="mb-6">
+        <h1 className="text-3xl font-bold text-gray-800">Healthcare Stocks Dashboard</h1>
       </header>
+      
+      <div className="grid grid-cols-4 gap-4">
+        {/* Main chart area - spans 3 columns */}
+        <div className="col-span-3 bg-white rounded-lg shadow p-4">
+          <StockChart />
+        </div>
+        
+        {/* Stock list - takes 1 column */}
+        <div className="col-span-1 bg-white rounded-lg shadow">
+          <StockList />
+        </div>
+        
+        {/* Sentiment analysis - spans 3 columns */}
+        <div className="col-span-3 bg-white rounded-lg shadow p-4">
+          <SentimentAnalysis />
+        </div>
+        
+        {/* Empty space for future use */}
+        <div className="col-span-1 bg-white rounded-lg shadow p-4">
+          {/* Future content */}
+        </div>
+      </div>
     </div>
   );
 }
